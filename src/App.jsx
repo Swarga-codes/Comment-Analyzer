25e09265f4ff5b9f11c009ce361929455a22bd65
Swarga-codes <sd3526@srmist.edu.in>
2024-02-13 20:40:35 +0530

    filtered positive and negative comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,66 @@
-import { useState,useEffect } from 'react'
+import { useState,useEffect, useRef } from 'react'
 import '@tensorflow/tfjs'
-import '@tensorflow-models/toxicity'
+import {load} from '@tensorflow-models/toxicity'
 import './App.css'
 
 function App() {
   const [commentInput,setCommentInput]=useState("")
   const [comments,setComments]=useState([])
-  function detectToxicity(){
+  const [modelHasLoaded,setModelHasLoaded]=useState(false)
+  const [positiveComments,setPositiveComments]=useState([])
+  const [negativeComments,setNegativeComments]=useState([])
+  const [verdict,setVerdict]=useState("")
+  const model=useRef(null)
+  async function loadModel(){
     // The minimum prediction confidence.
 const threshold = 0.9;
+// Set a state that indicates the model is being loaded...
+model.current = await load(threshold);
+setModelHasLoaded(true);
+// Set the state to false to let the user know that they can check the text
 
-// Load the model. Users optionally pass in a threshold and an array of
-// labels to include.
-toxicity.load(threshold).then(model => {
-  const sentences = ['you suck'];
-
-  model.classify(sentences).then(predictions => {
-  
-
-    console.log(predictions);
-    /*
-    prints:
-    {
-      "label": "identity_attack",
-      "results": [{
-        "probabilities": [0.9659664034843445, 0.03403361141681671],
-        "match": false
-      }]
-    },
-    {
-      "label": "insult",
-      "results": [{
-        "probabilities": [0.08124706149101257, 0.9187529683113098],
-        "match": true
-      }]
-    },
-    ...
-     */
-  });
-});
   }
-  function sendComment(){
+  async function sendComment(){
     if(!commentInput){
       return
+    }
+    const predictions=await model.current.classify([commentInput])
+    const predictionsFilter=predictions.filter(prediction=>prediction.results[0].match===true) 
+    if(predictionsFilter.length===0){
+    setPositiveComments([...positiveComments,commentInput])
+    }
+    else{
+      setNegativeComments([...negativeComments,{comment:commentInput,labels:predictionsFilter}])
     }
     setComments([...comments,commentInput])
     setCommentInput('')
   }
-//  useEffect(() => {
-
-//  }, [])
+ useEffect(() => {
+loadModel()
+ }, [])
  
 
   return (
     <>
     <h1>Comment Analyzer</h1>
+    <p>✅Positive Comments: {positiveComments.length}</p>
+    <p>❌Negative Comments: {negativeComments.length}</p>
+    <p>{verdict}</p>
    {comments?.map(
     (comment,idx)=>(
       <li key={idx}>{comment}</li>
     )
    )}
-   <form onSubmit={(e)=>{
+  {modelHasLoaded? <form onSubmit={(e)=>{
       e.preventDefault()
       sendComment()
     }}>
     <input type="text" value={commentInput} onChange={(e)=>setCommentInput(e.target.value)} placeholder='Enter your comment...'/>
     <button type="submit">Send</button>
     </form>
+    :
+    <p>Model is loading, please wait...</p>
+    }
     </>
   )
 }
